eb78762dd4aaedd434cc809bf03ecf4132370759
EfeKN <efe.kaan.fidanci@gmail.com>
2023-02-05 20:55:04 +0300

    Attacks added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// 1:12:24
+
 const canv = document.querySelector('canvas') 
 
 //Query Selector lets you find the first element that matches the CSS Selector
@@ -20,25 +22,47 @@ const c = canv.getContext('2d');
 const gravity = 0.7
 const speed = 5
 const hop = 20
+const cooldown = 1000
 
 class Sprite
 {
-    constructor({position, velocity}) {
+    constructor({position, velocity,color = 'red', offset}) {
         this.position = position
         this.velocity = velocity
         this.width = 50
         this.height = 150
         this.lk // = last_key
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y:this.position.y
+            },
+            offset,
+            width: 100,
+            height: 50
+        }
+        this.color = color
+        this.isAttacking
     }
 
     draw() {
-        c.fillStyle = 'red'
+        c.fillStyle = this.color
         c.fillRect(this.position.x,this.position.y,this.width,this.height)
+
+        //attack box
+        if(this.isAttacking)
+        {
+        c.fillStyle = 'blue'
+        c.fillRect(this.attackBox.position.x,this.attackBox.position.y,this.attackBox.width,this.attackBox.height)
+        }
+        c.fillStyle = 'green'
     }
 
     update() {
         this.draw()
 
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
         //movement
         this.position.y += this.velocity.y
         this.position.x += this.velocity.x
@@ -53,17 +77,27 @@ class Sprite
             this.velocity.y += gravity
         }
     }
+
+    attack() {
+        this.isAttacking = true
+        setTimeout(() => {
+            this.isAttacking = false
+        }, cooldown)
+    }
 }
 
 const player = new Sprite({
     position:{x:0, y:0},
-    velocity:{x:0, y:0}
+    velocity:{x:0, y:0},
+    offset: {x:0,y:0}
     })
 player.draw()
 
 const enemy = new Sprite({
     position:{x:974, y:0},
-    velocity:{x:0, y:0}
+    velocity:{x:0, y:0},
+    color:'yellow',
+    offset: {x:-50,y:0}
     })
 enemy.draw()
 
@@ -77,6 +111,15 @@ const keys =
     aUp: {pressed:false},
     aLeft: {pressed:false},
     aRight: {pressed:false}
+}
+
+function checkCollusion({rect1,rect2}) {
+    return (
+        rect1.attackBox.position.x + rect1.attackBox.width >= rect2.position.x &&
+        rect1.attackBox.position.x <= rect2.position.x + rect2.width &&
+        rect1.attackBox.position.y + rect1.attackBox.height >= rect2.position.y &&
+        rect1.attackBox.position.y <= rect2.position.y + rect2.height
+    )
 }
 
 //Animation Loop
@@ -106,6 +149,19 @@ function animate() {
     else if(keys.aRight.pressed && enemy.lk == 'ArrowRight') {
         enemy.velocity.x = speed
     }
+
+    //collusion detection
+    if (checkCollusion({rect1:player,rect2:enemy}) && player.isAttacking)
+    {
+        player.isAttacking = false
+        console.log('player_attacking')
+    }
+
+    if (checkCollusion({rect1:enemy,rect2:player}) && enemy.isAttacking)
+    {
+        enemy.isAttacking = false
+        console.log('enemy_attacking')
+    }
 }
 
 animate()
@@ -124,6 +180,9 @@ window.addEventListener('keydown', (event) => {
             keys.d.pressed = true
             player.lk = 'd'
             break
+        case ' ':
+            player.attack()
+            break
         case 'ArrowUp':
             enemy.velocity.y = -1 * hop
             break
@@ -134,6 +193,9 @@ window.addEventListener('keydown', (event) => {
         case 'ArrowRight':
             keys.aRight.pressed = true
             enemy.lk = 'ArrowRight'
+            break
+        case 'ArrowDown' :
+            enemy.isAttacking = true
             break
     }
 })
