0d04e14387fd3df6afa661a2fa35beb2c0f55121
EfeKN <efe.kaan.fidanci@gmail.com>
2023-02-05 18:15:29 +0300

    player and enemy created

--- index.js
+++ index.js
@@ -16,4 +16,61 @@ canv.width = 1024;
 canv.height = 576;
 
 const c = canv.getContext('2d');
-c.fillRect(0,0,1024,576)
+
+const gravity = 0.15
+class Sprite
+{
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.width = 50
+        this.height = 150
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x,this.position.y,this.width,this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y
+
+        //check y-bounds
+        if(this.position.y + this.height + this.velocity.y >= canv.height)
+        {
+            this.velocity.y = 0
+        }
+        else
+        {
+            this.velocity.y += gravity
+        }
+    }
+}
+
+const player = new Sprite({
+    position:{x:0, y:0},
+    velocity:{x:0, y:0}
+    })
+player.draw()
+
+const enemy = new Sprite({
+    position:{x:974, y:0},
+    velocity:{x:0, y:0}
+    })
+enemy.draw()
+
+console.log(player)
+
+//Animation Loop
+function animate() {
+    window.requestAnimationFrame(animate) // ---> infinite loop
+    c.clearRect(0,0,canv.width,canv.height)
+    c.fillStyle = 'black'
+    c.fillRect(0,0,canv.width,canv.height)
+    c.fillStyle = 'red'
+    player.update()
+    enemy.update()
+}
+
+animate()
